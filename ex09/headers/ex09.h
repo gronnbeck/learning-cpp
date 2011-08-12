71fdc03262bfbbe477786680986f4e230b616db9
Ken Grønnbeck <kengroenn@gmail.com>
2011-08-12 18:12:19 +0200

    ex09 part 1

---
+++ ex09/headers/ex09.h
@@ -0,0 +1,14 @@
+//
+//  ex09.h
+//  Ovinger C++
+//
+//  Created by Ken Grønnbeck on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Ovinger_C___ex09_h
+#define Ovinger_C___ex09_h
+
+
+
+#endif
---
+++ ex09/source/shuffle.cpp
@@ -0,0 +1,9 @@
+//
+//  shuffle.cpp
+//  Ovinger C++
+//
+//  Created by Ken Grønnbeck on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
